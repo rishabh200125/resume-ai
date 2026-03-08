4b88e1ab63928bd53722d7783f93834abb54f638
Rishabh Sahay <rishabhsahaya@gmail.com>
2026-03-08 19:38:54 +0530

    setup resume download

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
-
+import Protected from "./features/auth/components/Protected";
+import Home from "./features/interview/pages/Home";
+import Interview from "./features/interview/pages/Interview";
 
 export const router = createBrowserRouter([
     {
@@ -16,5 +18,9 @@ export const router = createBrowserRouter([
         path: "/",
         element: <Protected><Home /></Protected>
     },
+    {
+        path:"/interview/:interviewId",
+        element: <Protected><Interview /></Protected>
+    }
 
 ])
